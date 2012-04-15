0b0d7e0d25ca4f34482056c165437e9abffb0537
Shahid Ahmad <sha_online85@yahoo.com>
2012-04-15 03:57:41 +0300

    Update checkreminder.php

--- checkreminder.php
+++ checkreminder.php
@@ -4,31 +4,28 @@
 include('connect.php');
 $subject = 'Reminder';
 $to = ''; // an email address is required to be given here.
-$message = '';
 $header = 'From: Reminder System \nContent-Type: text/plain';
 
 // the following query selects only those reminders which are set today
-$Reminder_Result = $sql->dbQuery("select * from reminders where date='".date('Y-m-d')."'"); 
+$Reminder_Result = $sql->dbQuery("select * from reminders where date='".date('Y-m-d')."'" and flag = '1'); 
 $NumOfResult = $sql->dbNumRows($Reminder_Result);
 
 if($NumOfResult > 0) // check, if there is some result? 
 {
 	while($Reminder = $sql->dbFetchAssoc($Reminder_Result))
 	{
-		
 		$message .= "Event : ".$Reminder['title']."\n";
 		$message .= "Date : ".date('D M j, Y',strtotime($Reminder['date']))."\n";
 		$message .= $Reminder['description']."\n\n";
 		mail($to,$subject,$message,$header);
 		
 		$sql->dbQuery("update reminders set flag = '0' where id = '$Reminder[id]'"); // set the reminder to expired
-		echo "Reminder '".$Reminder['title']."' has been sent."; // print message
-		
+				
 	}
 }
 else
 {
-echo "There are no reminders set today, " .date('M j, Y'); // if there are no remindes set today.
+$Status_message = "There are no reminders set today,Today is : " .date('Y-m-d, H:i:s'); // if there are no active remindes set today.
 }
 
 ?> 
